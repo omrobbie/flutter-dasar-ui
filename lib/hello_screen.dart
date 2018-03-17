8bd921cfb0dc6302f81b45f249c2090518e301bc
omrobbie <putu.roby@yahoo.com>
2018-03-17 17:20:51 +0800

    minimum UI, masih error karna tidak pakai Scaffold

---
+++ lib/hello_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HelloScreen extends StatefulWidget {
+  @override
+  _HelloScreenState createState() => new _HelloScreenState();
+}
+
+class _HelloScreenState extends State<HelloScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.red,
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
+      child: Text('Hello Flutter'),
+    );
+  }
+}
