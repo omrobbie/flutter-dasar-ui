1a3f244901f33e216f96d1bbde3534803e2df008
omrobbie <putu.roby@yahoo.com>
2018-03-17 18:25:23 +0800

    tambahkan content didalam body dengan menggunakan Column

--- lib/material_screen.dart
+++ lib/material_screen.dart
@@ -19,9 +19,26 @@ class _MaterialScreenState extends State<MaterialScreen> {
       ],
     );
 
+    var display1 = Theme.of(context).textTheme.display1;
+
+    var _body = Column(
+      children: <Widget>[
+        Text(
+          'Flutter Indonesia',
+          style: display1,
+        ),
+        SizedBox(height: 16.0,),
+        Text(
+            'Deserunt veniam cupidatat qui eiusmod dolor minim culpa elit irure reprehenderit. Qui ipsum nostrud laborum dolor in cupidatat elit id id. Reprehenderit mollit elit do labore commodo ullamco adipisicing quis qui quis velit esse et. Ipsum ex velit velit magna excepteur. Fugiat velit eiusmod magna veniam velit. Cillum esse sint aliqua Lorem laboris do. Dolore aliqua fugiat cillum magna ipsum laboris anim exercitation mollit do id fugiat reprehenderit exercitation.'),
+      ],
+    );
+
     return new Scaffold(
       appBar: _appBar,
-      body: null,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: _body,
+      ),
     );
   }
 }
